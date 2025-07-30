69558f56a90630bd8f4b737d496330183ce3791e
Your Name <your-email@example.com>
2025-07-30 07:05:41 +0000

    Solve: Find the Index of the First Occurrence in a String

---
+++ solutions/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -0,0 +1,5 @@
+# Find the Index of the First Occurrence in a String
+# https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+# Solved: 2025-07-30
+
+# Solution to be added
