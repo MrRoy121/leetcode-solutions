1bfc90c7b72596d456a743397a2ec98d53b05b96
Your Name <your-email@example.com>
2025-10-22 07:05:45 +0000

    Solve: Replace Elements with Greatest Element on Right Side

---
+++ solutions/replace-elements-with-greatest-element-on-right-side.py
@@ -0,0 +1,5 @@
+# Replace Elements with Greatest Element on Right Side
+# https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
+# Solved: 2025-10-22
+
+# Solution to be added
