31b86071d3d174923f0a6713c0a2dbd3eb240450
Your Name <your-email@example.com>
2025-12-27 07:05:48 +0000

    Solve: Find Minimum Operations to Make All Elements Divisible by Three

---
+++ solutions/find-minimum-operations-to-make-all-elements-divisible-by-three.py
@@ -0,0 +1,5 @@
+# Find Minimum Operations to Make All Elements Divisible by Three
+# https://leetcode.com/problems/find-minimum-operations-to-make-all-elements-divisible-by-three/
+# Solved: 2025-12-27
+
+# Solution to be added
