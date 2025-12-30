3effbc25e1365851cdc277a9e75a4964fdb7edf3
Your Name <your-email@example.com>
2025-12-30 07:05:49 +0000

    Solve: Check If Digits Are Equal in String After Operations I

---
+++ solutions/check-if-digits-are-equal-in-string-after-operations-i.py
@@ -0,0 +1,5 @@
+# Check If Digits Are Equal in String After Operations I
+# https://leetcode.com/problems/check-if-digits-are-equal-in-string-after-operations-i/
+# Solved: 2025-12-30
+
+# Solution to be added
